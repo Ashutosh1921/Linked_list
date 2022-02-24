755814249423cad9dffa689ceab49bdd259addad
Ashutosh1921 <as7857139@gmail.com>
2022-02-24 15:35:39 +0530

    Update intro_1.cpp

--- double_list/intro_1.cpp
+++ double_list/intro_1.cpp
@@ -17,4 +17,11 @@ public:
 
 int main()
 {
+    node *head = new node(12);
+    node *one = new node(14);
+    node *two = new node(45);
+    head->next = one;
+    one->prev = head;
+    one->next = two;
+    two->prev = one;
 }
