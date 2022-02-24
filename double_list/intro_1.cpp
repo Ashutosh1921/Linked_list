43a2605990e53304b794871fa5ee52db60851994
Ashutosh1921 <as7857139@gmail.com>
2022-02-24 08:31:27 +0530

    Create intro_1.cpp

---
+++ double_list/intro_1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *prev;
+    node *next;
+    node(int d)
+    {
+        data = d;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+int main()
+{
+}
